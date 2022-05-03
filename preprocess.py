b5b2919d77432e5bbd60f77f89ac9ef5cd0cc78d
Sreya <haldersreya@gmail.com>
2022-05-03 09:59:07 -0700

    Changes to preprocess to work on our dataset

--- preprocess.py
+++ preprocess.py
@@ -19,10 +19,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 # Download model from TF Hub and check out inference code from GitHub
-!wget -q -O movenet_thunder.tflite https://tfhub.dev/google/lite-model/movenet/singlepose/thunder/tflite/float16/4?lite-format=tflite
-!git clone https://github.com/tensorflow/examples.git
-pose_sample_rpi_path = os.path.join(os.getcwd(), 'examples/lite/examples/pose_estimation/raspberry_pi')
-sys.path.append(pose_sample_rpi_path)
+# !wget -q -O movenet_thunder.tflite https://tfhub.dev/google/lite-model/movenet/singlepose/thunder/tflite/float16/4?lite-format=tflite
+# !git clone https://github.com/tensorflow/examples.git
+# pose_sample_rpi_path = os.path.join(os.getcwd(), 'examples/lite/examples/pose_estimation/raspberry_pi')
+# sys.path.append(pose_sample_rpi_path)
 
 # Load MoveNet Thunder model
 import utils
@@ -60,41 +60,6 @@ def detect(input_tensor, inference_count=3):
 
   return person
 
-#@title Functions to visualize the pose estimation results.
-
-def draw_prediction_on_image(
-    image, person, crop_region=None, close_figure=True,
-    keep_input_size=False):
-  """Draws the keypoint predictions on image.
- 
-  Args:
-    image: An numpy array with shape [height, width, channel] representing the
-      pixel values of the input image.
-    person: A person entity returned from the MoveNet.SinglePose model.
-    close_figure: Whether to close the plt figure after the function returns.
-    keep_input_size: Whether to keep the size of the input image.
- 
-  Returns:
-    An numpy array with shape [out_height, out_width, channel] representing the
-    image overlaid with keypoint predictions.
-  """
-  # Draw the detection result on top of the image.
-  image_np = utils.visualize(image, [person])
-  
-  # Plot the image with detection results.
-  height, width, channel = image.shape
-  aspect_ratio = float(width) / height
-  fig, ax = plt.subplots(figsize=(12 * aspect_ratio, 12))
-  im = ax.imshow(image_np)
- 
-  if close_figure:
-    plt.close(fig)
- 
-  if not keep_input_size:
-    image_np = utils.keep_aspect_ratio_resizer(image_np, (512, 512))
-
-  return image_np
-
 #@title Code to load the images, detect pose landmarks and save them into a CSV file
 
 class MoveNetPreprocessor(object):
@@ -102,7 +67,6 @@ class MoveNetPreprocessor(object):
  
   def __init__(self,
                images_in_folder,
-               images_out_folder,
                csvs_out_path):
     """Creates a preprocessor to detection pose from images and save as CSV.
 
@@ -119,15 +83,11 @@ class MoveNetPreprocessor(object):
             |______ 00000306.jpg
             |______ ...
         ...
-      images_out_folder: Path to write the images overlay with detected
-        landmarks. These images are useful when you need to debug accuracy
-        issues.
       csvs_out_path: Path to write the CSV containing the detected landmark
         coordinates and label of each image that can be used to train a pose
         classification model.
     """
     self._images_in_folder = images_in_folder
-    self._images_out_folder = images_out_folder
     self._csvs_out_path = csvs_out_path
     self._messages = []
 
@@ -136,7 +96,7 @@ class MoveNetPreprocessor(object):
  
     # Get list of pose classes and print image statistics
     self._pose_class_names = sorted(
-        [n for n in os.listdir(self._images_in_folder) if not n.startswith('.')]
+        ["1219", "1260", "1301", "1373", "1378", "1392", "1790", "1843", "1954", "2123", "489", "569", "581", "722", "731", "758", "786", "807", "832", "925"]
         )
     
   def process(self, per_pose_class_limit=None, detection_threshold=0.1):
@@ -153,12 +113,9 @@ class MoveNetPreprocessor(object):
       print('Preprocessing', pose_class_name, file=sys.stderr)
       
       # Paths for the pose class.
-      images_in_folder = os.path.join(self._images_in_folder, pose_class_name)
-      images_out_folder = os.path.join(self._images_out_folder, pose_class_name)
+      images_in_folder = os.path.join(self._images_in_folder, pose_class_name, 'rgb')
       csv_out_path = os.path.join(self._csvs_out_folder_per_class,
                                   pose_class_name + '.csv')
-      if not os.path.exists(images_out_folder):
-        os.makedirs(images_out_folder)
  
       # Detect landmarks in each image and write it to a CSV file
       with open(csv_out_path, 'w') as csv_out_file:
@@ -179,13 +136,13 @@ class MoveNetPreprocessor(object):
 
           try:
             image = tf.io.read_file(image_path)
-            image = tf.io.decode_jpeg(image)
+            image = tf.io.decode_image(image)
           except:
             self._messages.append('Skipped ' + image_path + '. Invalid image.')
             continue
           else:
             image = tf.io.read_file(image_path)
-            image = tf.io.decode_jpeg(image)
+            image = tf.io.decode_image(image)
             image_height, image_width, channel = image.shape
           
           # Skip images that isn't RGB because Movenet requires RGB images
@@ -205,15 +162,6 @@ class MoveNetPreprocessor(object):
             continue
 
           valid_image_count += 1
-
-          # Draw the prediction result on top of the image for debugging later
-          output_overlay = draw_prediction_on_image(
-              image.numpy().astype(np.uint8), person, 
-              close_figure=True, keep_input_size=True)
-        
-          # Write detection result into an image file
-          output_frame = cv2.cvtColor(output_overlay, cv2.COLOR_RGB2BGR)
-          cv2.imwrite(os.path.join(images_out_folder, image_name), output_frame)
         
           # Get landmarks and scale it to the same size as the input image
           pose_landmarks = np.array(
@@ -276,130 +224,19 @@ class MoveNetPreprocessor(object):
 
     return total_df
 
-#@title (Optional) Code snippet to try out the Movenet pose estimation logic
-
-#@markdown You can download an image from the internet, run the pose estimation logic on it and plot the detected landmarks on top of the input image. 
-
-#@markdown *Note: This code snippet is also useful for debugging when you encounter an image with bad pose classification accuracy. You can run pose estimation on the image and see if the detected landmarks look correct or not before investigating the pose classification logic.*
-
-test_image_url = "https://cdn.pixabay.com/photo/2017/03/03/17/30/yoga-2114512_960_720.jpg" #@param {type:"string"}
-!wget -O /tmp/image.jpeg {test_image_url}
-
-if len(test_image_url):
-  image = tf.io.read_file('/tmp/image.jpeg')
-  image = tf.io.decode_jpeg(image)
-  person = detect(image)
-  _ = draw_prediction_on_image(image.numpy(), person, crop_region=None, 
-                               close_figure=False, keep_input_size=True)
-
-
-is_skip_step_1 = False #@param ["False", "True"] {type:"raw"}
-use_custom_dataset = False #@param ["False", "True"] {type:"raw"}
-dataset_is_split = False #@param ["False", "True"] {type:"raw"}
-
 #@markdown Be sure you run this cell. It's hiding the `split_into_train_test()` function that's called in the next code block.
 
 import os
-import random
-import shutil
 
-def split_into_train_test(images_origin, images_dest, test_split):
-  """Splits a directory of sorted images into training and test sets.
+IMAGES_ROOT = 'YOUR_DATASET_DIR_NAME'
+# You can leave the rest alone:
+if not os.path.isdir(IMAGES_ROOT):
+  raise Exception("dataset_in is not a valid directory")
 
-  Args:
-    images_origin: Path to the directory with your images. This directory
-      must include subdirectories for each of your labeled classes. For example:
-      yoga_poses/
-      |__ downdog/
-          |______ 00000128.jpg
-          |______ 00000181.jpg
-          |______ ...
-      |__ goddess/
-          |______ 00000243.jpg
-          |______ 00000306.jpg
-          |______ ...
-      ...
-    images_dest: Path to a directory where you want the split dataset to be
-      saved. The results looks like this:
-      split_yoga_poses/
-      |__ train/
-          |__ downdog/
-              |______ 00000128.jpg
-              |______ ...
-      |__ test/
-          |__ downdog/
-              |______ 00000181.jpg
-              |______ ...
-    test_split: Fraction of data to reserve for test (float between 0 and 1).
-  """
-  _, dirs, _ = next(os.walk(images_origin))
-
-  TRAIN_DIR = os.path.join(images_dest, 'train')
-  TEST_DIR = os.path.join(images_dest, 'test')
-  os.makedirs(TRAIN_DIR, exist_ok=True)
-  os.makedirs(TEST_DIR, exist_ok=True)
-
-  for dir in dirs:
-    # Get all filenames for this dir, filtered by filetype
-    filenames = os.listdir(os.path.join(images_origin, dir))
-    filenames = [os.path.join(images_origin, dir, f) for f in filenames if (
-        f.endswith('.png') or f.endswith('.jpg') or f.endswith('.jpeg') or f.endswith('.bmp'))]
-    # Shuffle the files, deterministically
-    filenames.sort()
-    random.seed(42)
-    random.shuffle(filenames)
-    # Divide them into train/test dirs
-    os.makedirs(os.path.join(TEST_DIR, dir), exist_ok=True)
-    os.makedirs(os.path.join(TRAIN_DIR, dir), exist_ok=True)
-    test_count = int(len(filenames) * test_split)
-    for i, file in enumerate(filenames):
-      if i < test_count:
-        destination = os.path.join(TEST_DIR, dir, os.path.split(file)[1])
-      else:
-        destination = os.path.join(TRAIN_DIR, dir, os.path.split(file)[1])
-      shutil.copyfile(file, destination)
-    print(f'Moved {test_count} of {len(filenames)} from class "{dir}" into test.')
-  print(f'Your split dataset is in "{images_dest}"')
-
-if use_custom_dataset:
-  # ATTENTION:
-  # You must edit these two lines to match your archive and images folder name:
-  # !tar -xf YOUR_DATASET_ARCHIVE_NAME.tar
-  !unzip -q YOUR_DATASET_ARCHIVE_NAME.zip
-  dataset_in = 'YOUR_DATASET_DIR_NAME'
-
-  # You can leave the rest alone:
-  if not os.path.isdir(dataset_in):
-    raise Exception("dataset_in is not a valid directory")
-  if dataset_is_split:
-    IMAGES_ROOT = dataset_in
-  else:
-    dataset_out = 'split_' + dataset_in
-    split_into_train_test(dataset_in, dataset_out, test_split=0.2)
-    IMAGES_ROOT = dataset_out
-
-if not is_skip_step_1:
-  images_in_train_folder = os.path.join(IMAGES_ROOT, 'train')
-  images_out_train_folder = 'poses_images_out_train'
-  csvs_out_train_path = 'train_data.csv'
-
-  preprocessor = MoveNetPreprocessor(
-      images_in_folder=images_in_train_folder,
-      images_out_folder=images_out_train_folder,
-      csvs_out_path=csvs_out_train_path,
-  )
-
-  preprocessor.process(per_pose_class_limit=None)
-
-if not is_skip_step_1:
-  images_in_test_folder = os.path.join(IMAGES_ROOT, 'test')
-  images_out_test_folder = 'poses_images_out_test'
-  csvs_out_test_path = 'test_data.csv'
-
-  preprocessor = MoveNetPreprocessor(
-      images_in_folder=images_in_test_folder,
-      images_out_folder=images_out_test_folder,
-      csvs_out_path=csvs_out_test_path,
-  )
-
-  preprocessor.process(per_pose_class_limit=None)
+images_in_train_folder = os.path.join(IMAGES_ROOT)
+csvs_out_train_path = 'train_data.csv'
+preprocessor = MoveNetPreprocessor(
+    images_in_folder=images_in_train_folder,
+    csvs_out_path=csvs_out_train_path,
+)
+preprocessor.process(per_pose_class_limit=None)
