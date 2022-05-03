43471e6eece57f4224ae2b59446f2b107835f378
Sreya <haldersreya@gmail.com>
2022-05-03 16:42:18 -0700

    preprocess labels too

--- preprocess.py
+++ preprocess.py
@@ -19,8 +19,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 # Download model from TF Hub and check out inference code from GitHub
-# !wget -q -O movenet_thunder.tflite https://tfhub.dev/google/lite-model/movenet/singlepose/thunder/tflite/float16/4?lite-format=tflite
-# !git clone https://github.com/tensorflow/examples.git
 pose_sample_rpi_path = os.path.join(os.getcwd(), '../examples/lite/examples/pose_estimation/raspberry_pi')
 sys.path.append(pose_sample_rpi_path)
 
@@ -116,66 +114,76 @@ class MoveNetPreprocessor(object):
       images_in_folder = os.path.join(self._images_in_folder, pose_class_name, 'rgb')
       csv_out_path = os.path.join(self._csvs_out_folder_per_class,
                                   pose_class_name + '.csv')
+      labels_path = os.path.join(self._images_in_folder, pose_class_name, 'labels.csv')
+      labels_out_path = os.path.join(self._csvs_out_folder_per_class,
+                                  pose_class_name + '_labels.csv')
  
       # Detect landmarks in each image and write it to a CSV file
       with open(csv_out_path, 'w') as csv_out_file:
-        csv_out_writer = csv.writer(csv_out_file, 
-                                    delimiter=',', 
-                                    quoting=csv.QUOTE_MINIMAL)
-        # Get list of images
-        image_names = sorted(
-            [n for n in os.listdir(images_in_folder) if not n.startswith('.')])
-        if per_pose_class_limit is not None:
-          image_names = image_names[:per_pose_class_limit]
+        with open(labels_path, 'r') as labels_file:
+          with open(labels_out_path, 'w') as labels_out_file:
+            csv_out_writer = csv.writer(csv_out_file, 
+                                        delimiter=',', 
+                                        quoting=csv.QUOTE_MINIMAL)
+            csv_reader = csv.reader(labels_file)
+            labels_out_writer = csv.writer(labels_out_file, 
+                                           delimiter=',', 
+                                           quoting=csv.QUOTE_MINIMAL)
+            # Get list of images
+            image_names = sorted(
+                [n for n in os.listdir(images_in_folder) if not n.startswith('.')])
+            if per_pose_class_limit is not None:
+              image_names = image_names[:per_pose_class_limit]
 
-        valid_image_count = 0
- 
-        # Detect pose landmarks from each image
-        for image_name in tqdm.tqdm(image_names):
-          image_path = os.path.join(images_in_folder, image_name)
+            valid_image_count = 0
 
-          try:
-            image = tf.io.read_file(image_path)
-            image = tf.io.decode_image(image)
-          except:
-            self._messages.append('Skipped ' + image_path + '. Invalid image.')
-            continue
-          else:
-            image = tf.io.read_file(image_path)
-            image = tf.io.decode_image(image)
-            image_height, image_width, channel = image.shape
-          
-          # Skip images that isn't RGB because Movenet requires RGB images
-          if channel != 3:
-            self._messages.append('Skipped ' + image_path +
-                                  '. Image isn\'t in RGB format.')
-            continue
-          person = detect(image)
-          
-          # Save landmarks if all landmarks were detected
-          min_landmark_score = min(
-              [keypoint.score for keypoint in person.keypoints])
-          should_keep_image = min_landmark_score >= detection_threshold
-          if not should_keep_image:
-            self._messages.append('Skipped ' + image_path +
-                                  '. No pose was confidentlly detected.')
-            continue
+            # Detect pose landmarks from each image
+            for idx, image_name in enumerate(tqdm.tqdm(image_names)):
+              image_path = os.path.join(images_in_folder, image_name)
 
-          valid_image_count += 1
-        
-          # Get landmarks and scale it to the same size as the input image
-          pose_landmarks = np.array(
-              [[keypoint.coordinate.x, keypoint.coordinate.y, keypoint.score]
-                for keypoint in person.keypoints],
-              dtype=np.float32)
+              try:
+                image = tf.io.read_file(image_path)
+                image = tf.io.decode_image(image)
+              except:
+                self._messages.append('Skipped ' + image_path + '. Invalid image.')
+                continue
+              else:
+                image = tf.io.read_file(image_path)
+                image = tf.io.decode_image(image)
+                image_height, image_width, channel = image.shape
 
-          # Write the landmark coordinates to its per-class CSV file
-          coordinates = pose_landmarks.flatten().astype(np.str).tolist()
-          csv_out_writer.writerow([image_name] + coordinates)
-        if not valid_image_count:
-          raise RuntimeError(
-              'No valid images found for the "{}" class.'
-              .format(pose_class_name))
+              # Skip images that isn't RGB because Movenet requires RGB images
+              if channel != 3:
+                self._messages.append('Skipped ' + image_path +
+                                      '. Image isn\'t in RGB format.')
+                continue
+              person = detect(image)
+
+              # Save landmarks if all landmarks were detected
+              min_landmark_score = min(
+                  [keypoint.score for keypoint in person.keypoints])
+              should_keep_image = min_landmark_score >= detection_threshold
+              if not should_keep_image:
+                self._messages.append('Skipped ' + image_path +
+                                      '. No pose was confidentlly detected.')
+                continue
+
+              valid_image_count += 1
+
+              # Get landmarks and scale it to the same size as the input image
+              pose_landmarks = np.array(
+                  [[keypoint.coordinate.x, keypoint.coordinate.y, keypoint.score]
+                    for keypoint in person.keypoints],
+                  dtype=np.float32)
+
+              # Write the landmark coordinates to its per-class CSV file
+              coordinates = pose_landmarks.flatten().astype(np.str).tolist()
+              csv_out_writer.writerow([image_name] + coordinates)
+              labels_out_writer.writerow([image_name] + csv_reader[1])
+            if not valid_image_count:
+              raise RuntimeError(
+                  'No valid images found for the "{}" class.'
+                  .format(pose_class_name))
       
     # Print the error message collected during preprocessing.
     print('\n'.join(self._messages))
@@ -191,15 +199,19 @@ class MoveNetPreprocessor(object):
   def _all_landmarks_as_dataframe(self):
     """Merge all per-class CSVs into a single dataframe."""
     total_df = None
+    total_labels = None
     for class_index, class_name in enumerate(self._pose_class_names):
       csv_out_path = os.path.join(self._csvs_out_folder_per_class,
                                   class_name + '.csv')
+      labels_out_path = os.path.join(self._csvs_out_folder_per_class,
+                                  class_name + '_labels.csv')
       per_class_df = pd.read_csv(csv_out_path, header=None)
+      per_class_labels = pd.read_csv(labels_out_path, header=None)
       
       # Add the labels
       per_class_df['class_no'] = [class_index]*len(per_class_df)
       per_class_df['class_name'] = [class_name]*len(per_class_df)
-
+      
       # Append the folder name to the filename column (first column)
       per_class_df[per_class_df.columns[0]] = (os.path.join(class_name, '') 
         + per_class_df[per_class_df.columns[0]].astype(str))
@@ -207,9 +219,11 @@ class MoveNetPreprocessor(object):
       if total_df is None:
         # For the first class, assign its data to the total dataframe
         total_df = per_class_df
+        total_labels = per_class_labels
       else:
         # Concatenate each class's data into the total dataframe
         total_df = pd.concat([total_df, per_class_df], axis=0)
+        per_class_labels = pd.concat([total_labels, per_class_labels], axis=0)
  
     list_name = [[bodypart.name + '_x', bodypart.name + '_y', 
                   bodypart.name + '_score'] for bodypart in BodyPart] 
@@ -234,7 +248,7 @@ if not os.path.isdir(IMAGES_ROOT):
   raise Exception(IMAGES_ROOT, "is not a valid directory")
 
 images_in_train_folder = os.path.join(IMAGES_ROOT)
-csvs_out_train_path = 'train_data.csv'
+csvs_out_train_path = 'data.csv'
 preprocessor = MoveNetPreprocessor(
     images_in_folder=images_in_train_folder,
     csvs_out_path=csvs_out_train_path,
