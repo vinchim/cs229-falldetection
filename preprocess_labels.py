d4b928840165133ff48ce05bad1aaa691bf7653e
vinchim <104853650+vinchim@users.noreply.github.com>
2022-05-06 00:15:56 -0700

    Update preprocess_labels.py

--- preprocess_labels.py
+++ preprocess_labels.py
@@ -1,22 +1,6 @@
-import csv
-import cv2
-import itertools
+
 import numpy as np
 import pandas as pd
-import os
-import sys
-import tempfile
-import tqdm
-
-from matplotlib import pyplot as plt
-from matplotlib.collections import LineCollection
-
-import tensorflow as tf
-import tensorflow_hub as hub
-from tensorflow import keras
-
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 def main():
     data = pd.read_csv("../halde/cs229-falldetection/data.csv")
