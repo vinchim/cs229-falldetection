bc49ca8eae60beec3de6de7a4c063404db9eac8e
Sreya <haldersreya@gmail.com>
2022-05-16 21:05:46 -0700

    Preprocess labels

--- preprocess_labels.py
+++ preprocess_labels.py
@@ -8,7 +8,7 @@ def main():
     print(data)
     num_files, features = data.shape 
     labelsDict = {}
-    datasets = [1301, 1790, 722, 1378, 1392, 807, 758, 1843, 569, 1260, 489, 731, 1219, 1954, 581, 1176, 2123, 832, 786, 925]
+    datasets = [1301, 1790, 722, 1378, 1392, 807, 758, 1843, 569, 1260, 489, 731, 1219, 1954, 581, 1176, 2123, 832, 786, 925, 1373]
     path = "/data/xyz/labels.csv"
     answer = []
     #make all the dataset labels into dataframes and put them in a dictionary
