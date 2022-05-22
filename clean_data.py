8c5ed1e977eb9b32d9d447f437fa1cb8b46d5814
Sreya <haldersreya@gmail.com>
2022-05-22 13:41:04 -0700

    fixing preprocess labels

--- clean_data.py
+++ clean_data.py
@@ -14,7 +14,9 @@ def main():
             # extract the file name
             # example: dataset/rgb_0001.png
             file = row[0]
+            print(file)
             names = file.split("/", 1)
+            print(names)
             group = int(names[0])
             index = int(names[1].split("_", 1)[1].split(".",1)[0])
             if index <= group:
