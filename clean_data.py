2177f44465fb3f6e5f8d09b0597a20b69a6aff40
Sreya <haldersreya@gmail.com>
2022-05-22 13:37:43 -0700

    fixing preprocess labels

---
+++ clean_data.py
@@ -0,0 +1,24 @@
+
+import numpy as np
+import pandas as pd
+import csv
+
+def main():
+    data = pd.read_csv("data.csv")
+    print(data)
+
+    with open('data_clean.csv', 'wb') as out:
+        writer = csv.writer(out)
+        # go through each row of the data
+        for row in data.iterrows():
+            # extract the file name
+            # example: dataset/rgb_0001.png
+            file = row[1]["file_name"]
+            names = file.split("/", 1)
+            group = names[0]
+            index = int(names[1].split("_", 1)[1].split(".",1)[0])
+            if index <= group:
+                writer.writerow(row)
+
+if __name__ == "__main__":
+    main()
