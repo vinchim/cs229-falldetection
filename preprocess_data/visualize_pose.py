01cc916f52b9aa1b0fabf3501bd9684e23639944
Sreya <haldersreya@gmail.com>
2022-05-27 10:13:53 -0700

    visualize

---
+++ preprocess_data/visualize_pose.py
@@ -0,0 +1,100 @@
+import csv
+import cv2
+import itertools
+import numpy as np
+import pandas as pd
+import os
+import sys
+import tempfile
+import tqdm
+
+from matplotlib import pyplot as plt
+from matplotlib.collections import LineCollection
+
+import tensorflow as tf
+import tensorflow_hub as hub
+from tensorflow import keras
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+# Download model from TF Hub and check out inference code from GitHub
+pose_sample_rpi_path = os.path.join(os.getcwd(), '../../examples/lite/examples/pose_estimation/raspberry_pi')
+sys.path.append(pose_sample_rpi_path)
+
+# Load MoveNet Thunder model
+import utils
+from data import BodyPart
+from ml import Movenet
+movenet = Movenet('../movenet_thunder')
+
+# Define function to run pose estimation using MoveNet Thunder.
+# You'll apply MoveNet's cropping algorithm and run inference multiple times on
+# the input image to improve pose estimation accuracy.
+def detect(input_tensor, inference_count=3):
+  """Runs detection on an input image.
+ 
+  Args:
+    input_tensor: A [height, width, 3] Tensor of type tf.float32.
+      Note that height and width can be anything since the image will be
+      immediately resized according to the needs of the model within this
+      function.
+    inference_count: Number of times the model should run repeatly on the
+      same input image to improve detection accuracy.
+ 
+  Returns:
+    A Person entity detected by the MoveNet.SinglePose.
+  """
+  image_height, image_width, channel = input_tensor.shape
+ 
+  # Detect pose using the full input image
+  movenet.detect(input_tensor.numpy(), reset_crop_region=True)
+ 
+  # Repeatedly using previous detection result to identify the region of
+  # interest and only croping that region to improve detection accuracy
+  for _ in range(inference_count - 1):
+    person = movenet.detect(input_tensor.numpy(), 
+                            reset_crop_region=False)
+
+  return person
+
+#@title Functions to visualize the pose estimation results.
+
+def draw_prediction_on_image(
+    image, person, crop_region=None, close_figure=True,
+    keep_input_size=False):
+  """Draws the keypoint predictions on image.
+ 
+  Args:
+    image: An numpy array with shape [height, width, channel] representing the
+      pixel values of the input image.
+    person: A person entity returned from the MoveNet.SinglePose model.
+    close_figure: Whether to close the plt figure after the function returns.
+    keep_input_size: Whether to keep the size of the input image.
+ 
+  Returns:
+    An numpy array with shape [out_height, out_width, channel] representing the
+    image overlaid with keypoint predictions.
+  """
+  # Draw the detection result on top of the image.
+  image_np = utils.visualize(image, [person])
+  
+  # Plot the image with detection results.
+  height, width, channel = image.shape
+  aspect_ratio = float(width) / height
+  fig, ax = plt.subplots(figsize=(12 * aspect_ratio, 12))
+  im = ax.imshow(image_np)
+ 
+  if close_figure:
+    plt.close(fig)
+ 
+  if not keep_input_size:
+    image_np = utils.keep_aspect_ratio_resizer(image_np, (512, 512))
+
+  return image_np
+
+image = tf.io.read_file('images/rgb_0115.png')
+image = tf.io.decode_jpeg(image)
+person = detect(image)
+_ = draw_prediction_on_image(image.numpy(), person, crop_region=None, 
+                               close_figure=False, keep_input_size=True)
